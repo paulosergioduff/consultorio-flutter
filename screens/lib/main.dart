b3b45e0bd6382eb44e04e02c2fc50f74786bf072
paulosergioduff <paulosergioduff@gmail.com>
2021-02-25 21:39:41 -0300

    Todos os pacotes airbnb clone importados no main

--- screens/lib/main.dart
+++ screens/lib/main.dart
@@ -1,11 +1,16 @@
 // Pacotes airbnb clone
 import 'dart:ui';
+import 'package:screens/Routes/Ongoing.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:screens/Routes/HomePage.dart';
 import 'Constants/Constants.dart';
+import 'Routes/login.dart';
+// Fim dos pacotes airbnb
 
-import 'package:flutter/material.dart';
+// Pacote para chamar tela de abertura
 import 'splash.dart';
 
 import 'constants.dart';
