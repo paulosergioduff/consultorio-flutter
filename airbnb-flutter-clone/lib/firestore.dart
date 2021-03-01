df4b0d67adb941503ab31891e31b90f9f9508a87
paulosergioduff <paulosergioduff@gmail.com>
2021-03-01 10:41:40 -0300

    Notação xrud com CRUD da documentação

--- airbnb-flutter-clone/lib/firestore.dart
+++ airbnb-flutter-clone/lib/firestore.dart
@@ -18,6 +18,8 @@ class AddUser extends StatelessWidget {
 
     Future<void> addUser() {
       // Call the user's CollectionReference to add a new user
+
+      // Area de operação de CRUD (ABAIXO) <-- Verificar XRUD note
       return users
           .add({
             'full_name': fullName, // John Doe
@@ -26,6 +28,7 @@ class AddUser extends StatelessWidget {
           })
           .then((value) => print("User Added"))
           .catchError((error) => print("Failed to add user: $error"));
+      // Area de operação CRUD (ACIMA) <-- Verificar XRUD note
     }
 
     return TextButton(
@@ -36,3 +39,81 @@ class AddUser extends StatelessWidget {
     );
   }
 }
+
+class GetUserName extends StatelessWidget {
+  final String documentId;
+
+  GetUserName(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(documentId).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data.data();
+          return Text("Full Name: ${data['full_name']} ${data['last_name']}");
+        }
+
+        return Text("loading");
+      },
+    );
+  }
+}
+
+/*################ XRUD NOTE #################################
+
+1) Adcionar documento (código automático)
+
+return users
+          .add({
+            'full_name': fullName, // John Doe
+            'company': company, // Stokes and Sons
+            'age': age // 42
+          })
+          .then((value) => print("User Added"))
+          .catchError((error) => print("Failed to add user: $error"));
+
+
+2) Adcionar documento com nome
+
+return users
+    .doc('ABC123')
+    .set({
+      'full_name': "Mary Jane",
+      'age': 18
+    })
+    .then((value) => print("User Added"))
+    .catchError((error) => print("Failed to add user: $error"));
+
+
+3) Atualizando campos de documento
+
+return users
+    .doc('ABC123')
+    .update({'company': 'Stokes and Sons'})
+    .then((value) => print("User Updated"))
+    .catchError((error) => print("Failed to update user: $error"));
+
+4) Apagando documentos
+
+return users
+    .doc('ABC123')
+    .delete()
+    .then((value) => print("User Deleted"))
+    .catchError((error) => print("Failed to delete user: $error"));
+
+5) Lendo usuário -> Estudar documentação dado ao número de opções. 
+Classe de teste neste documento GetUserName
+
+
+############################### FIM DA NOTAÇÃO XRUD #####################
+
+*/
