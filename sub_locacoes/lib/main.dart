3e02202be8ba2f0fac6ca5327c61d3d62edef7b6
paulosergioduff <paulosergioduff@gmail.com>
2021-03-05 15:52:28 -0300

    "Todos os pacotes do clone airbnb compilados"

--- sub_locacoes/lib/main.dart
+++ sub_locacoes/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); //Enviando commit
 }
 
 class MyApp extends StatelessWidget {
